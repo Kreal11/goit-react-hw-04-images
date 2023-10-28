6b67b9e94992bdb9003585178d328d994b756ed1
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-28 19:22:36 +0300

    deleted comments

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -10,17 +10,6 @@ export const StyledWrapperOverlay = styled.div`
   align-items: center;
   background-color: rgba(0, 0, 0, 0.5);
   backdrop-filter: blur(10px);
-  /* position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  /* justify-content: center; */
-  /* align-items: center; */
-  /* background-color: rgba(0, 0, 0, 0.8);
-  z-index: 3;  */
 `;
 
 export const StyledWrapperModal = styled.div`
@@ -36,7 +25,6 @@ export const StyledWrapperModal = styled.div`
     width: 100%;
     max-height: 500px;
     object-fit: contain;
-    /* aspect-ratio: 16/9; */
   }
   button {
     color: black;
@@ -49,24 +37,4 @@ export const StyledWrapperModal = styled.div`
     font-size: 20px;
     cursor: pointer;
   }
-
-  /* max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 48px);
-  overflow: hidden;
-  position: relative;
-  top: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  button {
-    position: relative;
-    top: 30px;
-    left: 200px;
-  }
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    aspect-ratio: 16/9;
-  } */
 `;
